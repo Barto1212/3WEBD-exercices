a3a9c6d1f0ad40c02d7394d382473d5c059012bd
Loic Barthomeuf Ippon <lbarthomeuf@ippon.fr>
2024-02-20 09:06:23 +0100

    debut router

--- day3/optimise/src/App.tsx
+++ day3/optimise/src/App.tsx
@@ -1,47 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { useFetch } from "./useFetch";
-import ListItem from "./ListItem";
-import { cities } from "./cities";
-
-export type City = {
-  nom: string;
-  code: string;
-};
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
 
 function App() {
-  const sumCP = (comm: City[]): number => {
-    const initialValue = 0;
-    const sumWithInitial = comm.reduce(
-      (accumulator, currentValue) => accumulator + parseInt(currentValue.code),
-      initialValue
-    );
-
-    return sumWithInitial;
-  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>La somme de tous les CP = {sumCP(cities)}</h1>
-      <div className="card">
+      <BrowserRouter>
         <ul>
-          {cities.map((city) => (
-            <ListItem key={city.code} city={city} />
-          ))}
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
         </ul>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Routes>
+          <Route element={<Home />} path="/"></Route>
+          <Route element={<About />} path="/about"></Route>
+          <Route element={<Contact />} path="/contact"></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
